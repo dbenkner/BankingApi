da5ead39775cc5ee361bc9f083751b5d6df7ec27
David Benkner <d.benkner@gmail.com>
2023-09-13 14:24:18 -0400

    quick update

---
+++ BankingApi/Migrations/20230913180812_added account balance.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace BankingApi.Migrations
+{
+    /// <inheritdoc />
+    public partial class addedaccountbalance : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<decimal>(
+                name: "Balance",
+                table: "Accounts",
+                type: "decimal(11,2)",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Balance",
+                table: "Accounts");
+        }
+    }
+}
